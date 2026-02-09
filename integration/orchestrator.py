887a42630331bedbf6114f3cf82b07cf4a587fff
Shreyas Raorane <raorane@seas.upenn.edu>
2026-02-08 22:48:55 -0500

    variable initialization

--- integration/orchestrator.py
+++ integration/orchestrator.py
@@ -1,4 +1,37 @@
 import os
+from typing import Tuple
+
+class IntegrationOrchestrator:
+    """
+    Main Integration Orchestrator
+
+    - Run GCBBA to get task assignments
+    - Assignments are sent to AgentState for execution
+    - Collision Avoidance called for Path Planning and Replanning
+    - Step simulation forward and update AgentState with new positions and task statuses
+    - Trigger GCBBA replanning at specified intervals or when certain conditions are met (e.g. task completion, new tasks added, etc.)
+    """
+    
+    def __init__(self, 
+                 config_path: str, 
+                 tasks_per_induct_station: int = 10,
+                 comm_range: float = 30,
+                 sp_lim: Tuple[float, float] = (1.0, 5.0),
+                 rerun_interval: int = 10,
+                 stuck_threshold: int = 5,
+                 prediction_horizon: int = 5,
+                 max_plan_time: int = 400,
+                 ) -> None:
+        self.config_path = config_path
+        self.tasks_per_induct_station = tasks_per_induct_station
+        self.comm_range = comm_range
+        self.sp_lim = sp_lim
+        self.rerun_interval = rerun_interval
+        self.stuck_threshold = stuck_threshold
+        self.prediction_horizon = prediction_horizon
+        self.max_plan_time = max_plan_time
+
+        
 
 if __name__ == "__main__":
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
