17eb011c3ce434d8c6b1641950ca570326f7c506
Shreyas Raorane <raorane@seas.upenn.edu>
2026-02-08 22:22:29 -0500

    started working on integration orchestrator

---
+++ integration/orchestrator.py
@@ -0,0 +1,5 @@
+import os
+
+if __name__ == "__main__":
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    config_path = os.path.join(PROJECT_ROOT, "..", "config", "gridworld_warehouse_small.yaml")
