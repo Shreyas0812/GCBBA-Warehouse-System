f041e4e704e27214232c1a83fb3a032bdfb56b88
Shreyas Raorane <raorane@seas.upenn.edu>
2026-03-15 19:05:21 -0400

    init compelted

--- baselines/DMCHBA_Orchestrator.py
+++ baselines/DMCHBA_Orchestrator.py
@@ -31,3 +31,50 @@ as the cost matrix may become outdated or incomplete.
  
 Interface matches GCBBA_Orchestrator for drop-in replacement.
 """
+
+import time
+import numpy as np
+import networkx as nx
+from math import ceil
+from scipy.optimize import linear_sum_assignment
+
+from gcbba.GCBBA_Task import GCBBA_Task
+
+
+class DMCHBA_Orchestrator:
+    """Distributed Matching-by-Clones Hungarian-Based Algorithm Orchestrator for warehouse task allocation."""
+
+    def __init__(self, G, D, char_t, char_a, Lt=1, metric="RTP", task_ids=None, grid_map=None):
+        self.G = G
+        self.D = D
+        self.char_t = char_t
+        self.char_a = char_a
+        self.Lt = Lt
+        self.metric = metric
+        self.task_ids = task_ids if task_ids is not None else list(range(len(char_t)))
+        self.grid_map = grid_map
+
+        self.na = G.shape[0]
+        self.nt = len(char_t)
+
+        self.start_time = time.perf_counter()
+
+        # Initialize Agent
+        self.agent_pos = []
+        self.agent_speed = []
+        self.agent_pos_grid = []
+        for i in range(self.na):
+            pos = np.array(self.char_a[i][:3])
+            speed = self.char_a[i][3]
+            self.agent_pos.append(pos)
+            self.agent_speed.append(speed)
+            if self.grid_map is not None:
+                self.agent_pos_grid.append(self.grid_map.continuous_to_grid(*pos))
+            else:
+                self.agent_pos_grid.append(None)
+        
+        # Tracking 
+        self.assig_history = []
+        self.bid_history = []
+        self.max_times = []
+
