c1fafa4a661a8db1b83c1b4187f1922840aa11a7
Shreyas Raorane <raorane@seas.upenn.edu>
2026-03-15 18:44:32 -0400

    new baseline to be added

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -1,3 +1,4 @@
 # Baselines for thesis comparison
 # - SGA_Orchestrator: Centralized Sequential Greedy Algorithm
 # - CBBA_Agent + CBBA_Orchestrator: Standard CBBA (Choi et al. 2009)
+# - DMCHBA_Orchestrator: Distributed Matching-by-Clones Hungarian (Samiei & Sun, IEEE T-RO 2024)
